d4967100aa31cb5199a316b67656d509615be22a
Matthew Pawley <matthewpawley@btinternet.com>
2024-04-12 16:05:04 +0100

    update at gameweek 33

--- automated_los.R
+++ automated_los.R
@@ -1,6 +1,7 @@
 rm(list = ls())
 
 library(tidyverse)
+library(magrittr)
 library(fplr)
 library(regista)
 library(stringr)
@@ -13,14 +14,19 @@ options(dplyr.summarise.inform = FALSE)
 sapply(list.files("R", full.names = TRUE, recursive = TRUE), source)
 
 # manually input these parameters
-current_gameweek <- 29
-exclude_ids <- c(294) # exclude certain players from selection, e.g. long-term injured
-prev_selected_ids <- c(82, 85, 86) # these players will be unavailable except in wildcard week
+current_gameweek <- 32
+exclude_ids <- c(294, 60) # exclude certain players from selection, e.g. long-term injured
+gw_30_ids <- c(308, 362, 412) # Salah, Palmer, Gordon
+gw_31_ids <- c(293, 85, 19) # Darwin Nunez, Solanke, Saka
+gw_32_ids <- c() # Son, Nicolas Jackson, Matheus Cunha
+prev_selected_ids <- c(gw_30_ids, # these players will be unavailable except in wildcard week
+                       gw_31_ids) 
 min_gi <- 4
 min_minutes <- 800
-greedy_k_vals <- 4:6
+greedy_k_vals <- 4:8
 
-sapply(prev_selected_ids, function(id) player_hash[[as.character(id)]])
+sapply(exclude_ids, function(id) player_hash[[as.character(id)]])
+sapply(prev_selected_ids, function(id) player_hash[[as.character(id)]]) %>% matrix(ncol = 3, byrow = TRUE)
 
 # fit DC model and predict future scores
 pred_scores <- predict_scorelines(current_gameweek)
@@ -30,7 +36,7 @@ los_simplex <- fit_los_simplex(los_pool, current_gameweek)
 # compute goal involvement probabilities for each player/gameweek
 gw_probs <- get_los_gw_probs(pred_scores, los_simplex)
 # find best selection
-my_los <- optimise_los_selection(nreps = 1000, gw_probs, greedy_k = greedy_k_vals, prev_selected_ids, plot_surve_probs = TRUE)
+my_los <- optimise_los_selection(nreps = 2000, gw_probs, greedy_k = greedy_k_vals, prev_selected_ids, plot_surve_probs = TRUE)
 # print squad in more readable format
 matrix(my_los$squad$name, ncol = 3, byrow = TRUE, dimnames = list(current_gameweek:38, paste("Player", 1:3)))
 # save squad and probabilities
